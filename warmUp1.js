94afc80c78e1dbbfedbdec2ec6086688043ccb8d
B.Maher <bouzid.meher@gmail.com>
2019-10-16 09:28:45 +0100

    My first commit

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,9 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z = x + y;
+console.log(z); // here we display z in the console.
+
 // 2-calculate the area of a square using varaiables.
+var s = 2 // s is the side of the square 
+var area = s * s;
