bf7b997e5722a065a46c7159baaa746d669bb76f
muftah <muftah.elzawi@gmail.com>
2019-10-16 12:14:43 +0200

    firstWarmUp

--- warmUp1.js
+++ warmUp1.js
@@ -1,5 +1,7 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
-// 2-calculate the area of a square using varaiables.
 var z = x + y;
+// 2-calculate the area of a square using varaiables.
+ var squareLength = 0;
+ squareArea = squareLength * squareLength;
