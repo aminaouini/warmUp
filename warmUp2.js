baf19bca61acaf5976f46d982f0b1d3962c208b3
amineslaimia <medamineslaimia@gmail.com>
2019-10-17 18:23:20 +0200

    commit2

--- warmUp2.js
+++ warmUp2.js
@@ -2,3 +2,15 @@
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
 
+function greatestCommonDivisor (num1,num2){
+  if (num1 % num2 ===0 ){
+    return num2;
+  }
+return greatestCommonDivisor(num2,num1%num2); 
+}
+function sum(num1,num2){
+	if (num2===0){
+		return num1
+	}
+return sum(num1+1,num2-1)	;
+}
