4bca48d120c1dafd05d142ebf959e67e4fa21a76
amineaouini <amineaouini565@gmail.com>
2019-10-17 18:20:33 +0200

    my anwer

--- warmUp2.js
+++ warmUp2.js
@@ -2,3 +2,7 @@
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
 
+function sum(n1, n2){
+	if (n2>n1) return n1 + n2;
+	return n1 + sum(n1, n2 + 1)
+}
