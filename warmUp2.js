fec6d69520205d6c2c4d6116f2eb865437562102
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-10-17 09:50:16 +0100

    warm2

--- warmUp2.js
+++ warmUp2.js
@@ -1,4 +1,25 @@
 // 1- Using recursion Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
-// 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
+// 2-Write a function called sum that accepts two numbers as parameters,
+// and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
+
+
+function gcd(x , y) {
+
+}
+
+function inc(y) {
+    return y = y + 1;
+}
+function dec(x) {
+    return x = x - 1;
+}
+
+function sum(x , y) {
+    if (x === 0){
+        return y;
+    }
+    return sum(dec(x), inc(y))
+}
+
 
