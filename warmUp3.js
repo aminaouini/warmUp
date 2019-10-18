837063c6e608b7510653722b909bb3eeda552278
AnesMk <marzoukianas@gmail.com>
2019-10-18 18:35:05 +0200

    “warmUp3”

--- warmUp3.js
+++ warmUp3.js
@@ -6,3 +6,26 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+
+var people = ["hania", "fatma", "anis", "ali"];
+
+// 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
+
+function middle(array) {
+	return array[array.length/2];
+}
+
+// 3-Change all the numbers in the array to be multiplied by two for even indexes.
+function multiplied(array){
+	var resarr=[];
+	for (var i = 0; i < array.length; i++) {
+		if(i %2 === 0){
+			resarr.push(array[i] * 2);
+		}
+		else {
+		resarr.push(array[i]);
+		}
+	}
+	return resarr;
+}
