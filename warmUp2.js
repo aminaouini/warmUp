4ee565c918210287880a3166c6488f2e52e6b64e
fatima <onlyfefe03@gmail.com>
2019-10-19 00:02:33 +0300

    finish WarmUp

--- warmUp2.js
+++ warmUp2.js
@@ -2,3 +2,16 @@
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
 
+function gcd(a, b){
+	var divisor = Math.min (a, b);
+	var find = false;
+		while (find == false) {
+			if (((a % divisor) == 0) && ((b % divisor) == 0)){
+				return divisor;
+				find = true;
+			}
+			else {
+				divisor -= 1;
+	 }
+	}
+}
--- warmUp3.js
+++ warmUp3.js
@@ -3,6 +3,38 @@
 // -name of males in your class
 // -name of your class instructors 
 
-// 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
+
+var males = ['Ahmed', 'malik', 'belal'];
+var females     = ['Amira', 'Fooz', 'Rubaa']
+var instructors = ['Rgda','saife'];
+
+// 2-write a function that takes an array as an argument and returns
+// the element that is located in the middle of that array.
+function getMiddel(array){
+	var middle = '';
+	var i = 0;
+	while(i < array.length){
+		if(array.length%2 === 1){
+			  
+            middle = array[Math.floor(array.length/2)];
+		}
+		else if(array.length%2 === 0){
+			middle = array[array.length/2 -1] + ' ' + array[array.length/2];
+		}
+        i++;
+	}
+	return middle;
+
+}
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+function multipayed(array){
+	var i = 0;
+	while(i < array.length){
+	    if(array[i]%2 === 0){
+		array[i] = array[i] * 2;
+	  }	
+	  i++;
+  }
+	return array;
+}
