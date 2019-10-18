92fb3adaec73206b0ca71ba2cc41335d0eedc6b5
NianaNina <rahmanions@gmail.com>
2019-10-18 17:21:29 +0100

    “yourMessage”

--- warmUp3.js
+++ warmUp3.js
@@ -3,6 +3,27 @@
 // -name of males in your class
 // -name of your class instructors 
 
+[insaf, amira, houda, hania, rouba]
+[amine, dhia, mahdi, essam, hammam, ahmad]
+[seif, yousef, matt, tamara, omar]
+
+
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
+
+
+
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+function double(number){
+	var total[] = 0;
+	if (number >= 0 ){
+		total = number * 3;
+	}
+
+	return number *2;
+}
+
+
+
+
