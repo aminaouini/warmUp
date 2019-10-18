b80c7fde9ce26a7414ee1c71e53569feec48730f
AdamMomen <adammuman81@gmail.com>
2019-10-18 09:51:22 +0100

    This the answer of WarmUp3

--- warmUp3.js
+++ warmUp3.js
@@ -6,3 +6,26 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+//srart
+//////////----1
+var femaleNames = ['Fatima', 'Tamara', 'Zahra', 'Noor'];
+var maleNames = ['Ali', 'Adam', 'Hashem', 'Mohammed'];
+var instrNames = ['Yousef', 'Seif', 'Mohammed', 'Omar'];
+//////////----2
+function middleArray(array) {
+var	arrEvenIndex =[];
+
+		if(array.length % 2 == 0 ) {
+			arrEvenIndex.push(array[array.length  / 2] - 1) && arrEvenIndex.arrEvenIndex(array[(array.length / 2)])
+	} 
+	return arrEvenIndex 2 ; 
+}
+/////// ------3
+function middleArray(array) {
+var	arrEvenIndex =[];
+
+		if(array.length % 2 == 0 ) {
+			arrEvenIndex.push((array[array.length  / 2] - 1)  * 2) && arrEvenIndex.arrEvenIndex((array[(array.length / 2)])  * 2)
+	} 
+	return arrEvenIndex ; 
+//end
