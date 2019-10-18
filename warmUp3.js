4cca478e9f1d093410c42cb3cd8b3d0967993def
SofianSaleh <sofiane.saleh11@gmail.com>
2019-10-18 10:42:43 +0200

    third warmUp

--- warmUp3.js
+++ warmUp3.js
@@ -2,7 +2,25 @@
 // -name of females in your class
 // -name of males in your class
 // -name of your class instructors 
-
+var females = ['Ons', 'Ensaf', 'Hania'];
+var male = ['Abd Alrahman', 'Hashem', 'Ali', 'Malik', 'Adam'];
+var instructors = ['Tamara', 'Saif', 'Yousef','Matt'];
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
-
+function returns(array){
+	var i = array.length - 1;
+	if(i % 2 === 0){
+			return array[i/2]
+		}else {
+			i++;
+			return array[i/2] + ' ' + array[(i-2)/2];
+		}
+	}
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+var num = [1, 2, 3, 4];
+function Multiply(array){
+	for (var i = 0; i < array.length; i++){
+		if(i % 2 === 0){
+			array[i] = array[i] * 2;
+		}
+	}return array;
+}
