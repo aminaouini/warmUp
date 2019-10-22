f5bc3b5c8b8532466e9977249394e04c8801834e
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-10-22 10:00:38 +0100

    warm Up 5

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,60 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+
+// factory function
+function addPeople(name, age, gender, natinalty) {
+  return {
+    name: name,
+    age: age,
+    gender: gender,
+    natinalty: natinalty
+  }
+}
+
+var person1 = addPeople('Fooz Barakat', '24', 'female', 'Libyan');
+var person2 = addPeople('Amera Ahmed', '23', 'female', 'Libyan');
+
+
+// classmates
+var people = [];
+people.push(person1, person2);
+
+
+// displayFriend
+function displayFriend(object) {
+  return object.name + ', ' + object.age + ', ' + object.gender + ', ' + object.natinalty + '.'
+}
+
+
+// addFriend
+function addFriend(object) {
+  people.push(object);
+  return people;
+}
+
+
+// nbOfMale
+function nbOfMale(array) {
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {      
+      if ( array[i]['gender'] === 'male' ) {
+        count++;
+      }   
+  }
+  return count;
+}
+
+
+// searchMates
+function searchMates(string, array) {
+  var result = '';
+  for (var i = 0; i < array.length; i++) {
+    if ( array[i]['name'] === string ) {
+      result = array[i]['name'] + ', ' + array[i]['age'] + ', ' + array[i]['gender'] + '.';
+      return result;
+    }
+  }
+  return 'No match';
+}
