47087a9d0fd30649d45c07fccd28983c2089b1c1
amineslaimia <medamineslaimia@gmail.com>
2019-10-22 17:48:35 +0100

    warmup5

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,33 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+function makeClassmate (firstName, lastName, age, gender ){
+    var classMate ={
+        firstName:firstName,
+        lastName:lastName,
+        age:age,
+        gender,gender
+    }
+    return classMate;
+
+}
+
+
+function displayFriend(mate){
+    return 'my classmates"s name is ' +mate.firstName+' '+mate.lastName+' is a '+mate.gender+' and '+mate.age+' years old';
+}
+var classMates=[
+    {firstName:'amine',lastName:'slaimia',age:25,gender:'male'}
+]
+
+function addFriend (mate){
+    classMates.push(mate);
+}
+function searchMates (array, queryLastName){
+    for (var i in array){
+        if(array[i].lastName===queryLastName)
+        return array[i];
+        else return 'this mate is not found'
+    }
+
+}
