0c4bf371d60abb30341f9cc53de9418272c7b6f5
adanino2 <benabdelaaliadnen@gmail.com>
2019-10-22 18:30:00 +0100

    refactored

--- warmup5.js
+++ warmup5.js
@@ -1,9 +1,9 @@
 // 1-create a data model to represent your classmates 
 
-var classmate = {name:{first: "firstName", last: "lastName"} ,age: 0};
+var classmate = {name:{first: "firstName", last: "lastName"} ,age: 0, sex: "maleOrFemale"};
 //     -think of different attributes of your classmates? what do all of them have ?
 //     -create a factory function.
-function createThem(theName, firstName, lastName, age) {
+function createThem(theName, firstName, lastName, age, sex) {
 
 	var obj = {};
 	obj["name"] = {};
@@ -12,6 +12,7 @@ function createThem(theName, firstName, lastName, age) {
 	access["first"] = firstName;
 	access["last"] = lastName; 
 	obj["age"] = age;
+	obj["sex"] = sex;
 	//console.log(access["first"]);
 	//console.log(access["last"]);
 	//console.log(classmate["age"]);
@@ -19,22 +20,55 @@ function createThem(theName, firstName, lastName, age) {
 		theName[key] = obj[key];
 		console.log(obj[key]);
 	}
-
-	
-
 	return theName;
 
 };
 var adnen = {};
- createThem(adnen, "Adnen", "Ben ABdelaali", 27);
-
-createThem("Taha", "Zanzen", 20);
-createThem("Hamza","", 20);
-createThem("Mohamed Amine", "Khadraoui", 20);
+var taha = {};
+var hamza = {};
+var mohamed = {};
+createThem(adnen, "Adnen", "Ben Abdelaali", 27, "male"); 
+createThem(taha, "Taha", "Zanzen", 20, "male");
+createThem(hamza, "Hamza","", 20, "male");
+createThem(mohamed, "Mohamed Amine", "Khadraoui", 20, "male");
 //     -create an array to hold the classmates that you created and what you created to it .
-var classmates = [];  
+var classmates = [adnen, taha, hamza, mohamed];  
 //     -write a function called displayFriend that takes a mate as an argument and returns the important information in a readable way.
+function displayFriend(mate) {
+	var access = mate["name"]; 
+	
+	return "My First name is " + access["first"] +" ,my last name is : " + access["last"] + " , I am " + mate["age"] + " years old and I am a " + mate["sex"] + " ." 
+} 
 //     -write a function called addFriend that takes a mate as an argument and add it to you classMates arraya.
+function addFriend(mate, array) {
+	array[array.length] = mate; 
+	return array; 
+}
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
+function nbOfMale(array) {
+	var count = 0;
+	for(var i = 0 ; i < array.length; i++){
+		var access = array[i]
+		if(access["sex"] === "male") {
+			count += 1;
+		}
+	}
+	return count;
+}
 //     -Write a function searchMates that, given a query and an array of Mates,
+function searchMates(query, array) {
+	for(var i = 0; i < array.length; i++){
+		var check = array[i];
+		var myName = check["name"];
+		for (var key in myName){
+		    console.log(myName[key]);
+			if(query.toUpperCase() === myName[key].toUpperCase()) {
+				return array[i];
+			}	
+		}
+		
+	}
+	return "Your mate is lost ! Try again ";
+}
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
