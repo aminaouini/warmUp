33bbe67d494673e7a8d92d5a023c75b899c0fef5
H <H@h.com>
2019-10-22 21:21:25 +0200

    this is a refactor

--- warmup5.js
+++ warmup5.js
@@ -8,8 +8,6 @@
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 
-
-
 /*
 	- unfortuanetly i didn't study how to implement the data model but i know the concept,
 	so for the first point we need to make an object has let's say (name, age, talents, status, 'and so on') and then assign the values by 
@@ -20,3 +18,42 @@
 	counter will be + 1 and then will return the counter for us.
 	- searchMates take a name of a friend and it go through the object searching for the name been provided and return his/her infos.  
 */
+
+function makeClassMates(name, age, gender, talents) {
+	return {
+		name: name,
+		age: age,
+		gender: gender,
+		talents: talents
+	}
+}
+
+var arr = [
+	makeClassMates('hashem', '21', 'male', 'beatboxer'),
+	makeClassMates('malik', '23', 'male', 'clown'),
+	makeClassMates('ali', '18', 'female', 'loner'),
+];
+
+function displayFriend(mate) {
+	return  "name: " + mate.name + '\n' +
+			"age: "	 + mate.age + '\n' +
+			"gender: " + mate.gender + '\n' +
+			"talents: " + mate.talents
+}
+
+function addMate(mate) {
+	arr.push(mate);
+	
+	return arr;
+}
+
+function nbOfMale(mate) {
+	var count = 0;
+	for (var key in mate) {
+		if (mate.gender === 'male') {
+			count++;
+		}
+	}
+
+	return count;
+}
