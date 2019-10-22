681f127377ae83b31856b48b9030090a246fecec
Amera Ayad <amera.ayad.it@gmail.com>
2019-10-22 09:57:00 +0100

    warm up 5

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,47 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+function factoryClassMate(name, nationality, gender, note){
+    return {
+        name: name,
+        nationality: nationality,
+        gender: gender,
+        note: note
+    }
+}
+
+var classMates = [];
+classMates.push(factoryClassMate('Lina', 'Tunisian', 'Male', 'Loves dogs'));
+classMates.push(factoryClassMate('Maher', 'Tunisian', 'Male', 'so smart'));
+classMates.push(factoryClassMate('Fatima', 'Libyan', 'Female', 'my bff'));
+
+function displayFriend(friend){
+    return `Name: ${friend.name}, Nationality: ${friend.nationality}, Gender: ${friend.gender}, ${friend.note}`;
+}
+
+function addFriend(mate){
+    classMates.push(mate);
+    return `added ${displayFriend(mate)} successfully.`;
+}
+
+function nbOfMale(){
+    for(var i = 0; i < classMates.length; i++){
+        var count = 0;
+        if(classMates[i].gender === 'Male'){
+            ++count;
+        }
+    }
+    return count;
+}
+
+function searchMates(query){
+    for(var i = 0; i < classMates.length; i++){
+        for(var key in classMates[i]){
+            if(query.toLowerCase().includes(classMates[i][key].toLowerCase())){
+                return classMates[i];
+            }
+        }
+    }
+    return 'cant find any result.'
+}
