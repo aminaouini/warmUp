dacf236bb5cf95f9001e60b67e2fbcfc07a183e4
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-10-22 09:42:19 +0100

    solved warmup 5

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,48 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+function makeMate(name, age, sex, gender, education){
+	return {
+		name: name,
+		age: age,
+		sex: sex,
+		gender: gender,
+		education: education
+	};
+}
+
+
+var classMates = [
+	makeMate("Adam", 23, "male", "male", "Bachelor"),
+	makeMate("Hashem", 21, "male", "male", "Bachelor Student"),
+	makeMate("Essam", 18, "male", "male", "Community College Student"),
+	makeMate("Malik", 21, "male", "male", "Bachelor")	
+];
+
+
+function displayFriend(mate){
+	console.log(`My mate's name is ${mate.name}, he's ${mate.age} years old, he is ${mate.gender}`);
+}
+
+function addFriend(mate){
+	classmates.push(mate);
+}
+
+function nbOfMale(array){
+	return array.filter(function(element){
+		return element.gender === "male";
+	}).reduce(function(acc){
+		return ++acc;
+	}, 0)
+}
+
+function searchMates(array, query){
+	return array.filter(function(element){
+		for(var key in element){
+			if(element[key].indexOf(query)>= 0)
+				return true
+		}
+		return false;
+	})
+}
