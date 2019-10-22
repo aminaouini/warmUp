a1207d2d67df83cf253c555c137ae888886b8e1c
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-10-22 09:53:11 +0100

    Sloved all the problem

--- warmup5.js
+++ warmup5.js
@@ -1,9 +1,60 @@
 // 1-create a data model to represent your classmates
-//     -think of different attributes of your classmates? what do all of them have ?
-//     -create a factory function.
-//     -create an array to hold the classmates that you created and what you created to it .  
-//     -write a function called displayFriend that takes a mate as an argument and returns the important information in a readable way.
-//     -write a function called addFriend that takes a mate as an argument and add it to you classMates arraya.
-//     -calculate the number of male friends that your class have by writing a function called nbOfMale.
+
+
+function classmates (First_name, Last_name, Age, Gender, Education, City, Country){
+	return {
+		First_name: First_name,
+		Last_name: Last_name,
+		Age: Age,
+		Gender: Gender
+		Education: Education,
+		City: City,
+		Country: Country
+		}
+}
+
+
+
+var ClassMates = [];
+
+function displayFriend(classmate){
+	return classmate[First_name] + classmate[Last_name] + " is a " + classmate[Age] + " year old " + classmate[Gender] + " with a degree in " + classmate[Education] + " from " + classmate[City] + ", " + classmate[Country]
+}
+
+function addFriend (mate){
+	ClassMates.push(mate);
+}
+
+function nbOfMale (classmates){
+	var acc= []
+	for (var i = 0; i < classmates.length; i++) {
+	 if (classmates[i][Gender]=== "Male"){
+	 	acc.push(classmates[i]);
+	 }
+	}
+	return acc.length;
+}
+
+function searchMates(attr, array) {
+
+	var acc= []
+	for (var i = 0; i < array.length; i++) {
+	 for (key in array[i]) {
+	 	if (array[i][key] === attr){
+	 	acc.push(array[i]);
+	 	}
+	 }
+	}
+	return acc;
+}
 //     -Write a function searchMates that, given a query and an array of Mates,
-//           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+//  searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+
+
+
+
+
+
+
+
