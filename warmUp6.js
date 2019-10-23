c07d648747f1ec803520f916e6a174f62523fb17
AdamMomen <adammuman81@gmail.com>
2019-10-23 09:49:06 +0100

    WarmUp6 Solution

--- warmUp6.js
+++ warmUp6.js
@@ -6,7 +6,50 @@
  // your code is here
 }
 
+function mult(number) {
+
+var result = 1;
+var string = []; 
+var strIterator = 1 ;
+	for (var i = 1; i <= number; i++) {
+		result *=i;
+			}
+	//stating from here is the solution for question 2
+while(strIterator < number) {
+	if(strIterator === 1){
+		string.push(1)
+		strIterator++;
+			} else {
+				string.push(strIterator+ " "+ strIterator );
+			strIterator++;
+	}
+		return result;
+		return string.join(' ');
+}
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+ 
+
+ //The Impelentation in the first function
+ function mult(number) {
+
+var result = 1;
+var string = []; 
+var strIterator = 1 ;
+	for (var i = 1; i <= number; i++) {
+		result *=i;
+			}
+	
+while(strIterator < number) {
+	if(strIterator === 1){
+		string.push(1)
+		strIterator++;
+			} else {
+				string.push(strIterator+ " "+ strIterator );
+			strIterator++;
+	}
+		return result;
+		return string.join(' ');
+}
