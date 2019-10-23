b29729b734653db212d6c0ad63d64b542f78ebf5
muftah <muftah.elzawi@gmail.com>
2019-10-23 10:32:16 +0200

    sixPush

--- warmUp6.js
+++ warmUp6.js
@@ -2,11 +2,20 @@
  //starting from 0, e.g.:
  mult(3); // => 6
  mult(4); // => 24
- function mult(n) {
+ 
+function mult(n) {
  // your code is here
+ if (n === 0) {
+
+  return 1
+
+ }
+ return n * mult(n - 1)
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+
+// I'm so sorry the quation wasn't clear for me.
