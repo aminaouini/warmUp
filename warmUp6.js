2dea08a35172abd72580e7440ed80733d1051151
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-10-23 10:52:35 +0200

    WarmUp6

--- warmUp6.js
+++ warmUp6.js
@@ -3,10 +3,29 @@
  mult(3); // => 6
  mult(4); // => 24
  function mult(n) {
- // your code is here
+ var result = 1;
+
+ for(var i = 0; i <= n; i++) {
+ 	result  *= n--;
+ }
+
+ return result;
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+
+function buildAString(n) {
+	var result = '';
+	var i = 2;
+	result += 1;
+
+	while(i <= n) {
+		result += ' '+i +' '+i;
+		i++;
+	}
+
+	return result ;
+}
