0f6ebab7a99f3a55b3f140ecc681486103b35434
amineslaimia <medamineslaimia@gmail.com>
2019-10-23 09:39:45 +0100

    warmUp 6

--- warmUp6.js
+++ warmUp6.js
@@ -3,10 +3,23 @@
  mult(3); // => 6
  mult(4); // => 24
  function mult(n) {
- // your code is here
+   if (n === 1){
+      return 1;
+   }
+ return n * mult(n-1)
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+function stringNumber (n){
+    var result='';
+    var i =1;
+    while (i<= n){
+        result= result+i+(i+1);
+        i++;
+    }
+
+    return result;
+}
