8a91d5904e1280777a47a00c0613b31d825b474e
omarbara <omarbara@ymail.com>
2019-10-23 09:41:44 +0100

    wUp6

--- warmUp6.js
+++ warmUp6.js
@@ -4,9 +4,25 @@
  mult(4); // => 24
  function mult(n) {
  // your code is here
+ var total = total || 1;
+ if(n <= 0 ){
+ 	return total;
+ }
+ return total = n * mult(n-1);
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+
+function numberString (n) {
+	var str = '';
+	var count =1;
+   while(n > 0){
+   	str += count+' '+count+' ';
+   	n--;
+   	count++;
+   }
+	return str;
+}
