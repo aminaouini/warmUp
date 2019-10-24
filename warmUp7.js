84365ccd69eb93784776206af99f5a81c699f131
Quantum299 <firasamous2s4@gmail.com>
2019-10-24 10:51:19 +0200

    warmUp7/done

---
+++ warmUp7.js
@@ -0,0 +1,37 @@
+// 1-using + operator combine your partner first and last name .
+
+// 2-find if the number 13 is a multiple of 3 or not.
+
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+
+// 4-calculate your age in seconds.
+
+// your code is here
+ 
+
+ //  1  //
+ var partnerName = 'Jihad' + 'Abdeli' ;
+
+ // 2 //
+
+ function multiple (n) {
+ 	if ( n % 3 !=== 0 ) {
+ 		return 'n is not multiple by 3' ;
+ 	}
+ }
+
+ //  3  //
+
+var avg = 0;
+for(var i = 0; i < ages.length; i++) {
+    avg += ages[i];
+}
+var average = avg / ages.length;
+
+
+ // 4 //
+
+var myAge = 29 * 365 * 24 * 60 * 60 ;
+
+
+
