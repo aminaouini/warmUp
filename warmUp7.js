bd38c002b4422e4f8500cb4c56d889efafbb112c
amineslaimia <medamineslaimia@gmail.com>
2019-10-24 09:42:14 +0100

    warmup7

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,18 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+//1
+console.log('amine'+'slaimia');
+//2
+consol.log(13 % 3 === 0);
+//3
+function average () {
+    var arr= [13,14,13,15,16,17,19,13,16,15];
+   var sum=0;
+    for (var i in arr){
+        sum= sum + arr[i];
+    }
+    return sum/ arr.length;
+}
+//4
+console.log('my age is '+25*365*24*60+' seconds');
