0984ff0d97b87cfd4b6740e6fc7940cfc0ddef24
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-10-24 21:34:40 +0200

    warmup 7 done

--- warmUp7.js
+++ warmUp7.js
@@ -1,6 +1,22 @@
 // 1-using + operator combine your partner first and last name .
-// 2-find if the number 13 is a multiple of 3 or not.
-// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
-// 4-calculate your age in seconds.
+function pairPairName(first, last) {
+	return first + " " + last;
+}
 
-// your code is here
+// 2-find if the number 13 is a multiple of 3 or not.
+function isThirteenAMultipleOfThree(){
+	return 13%3===0;
+}
+
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+function averageAge(array){
+	return array.reduce(function(acc, value){
+		return acc+value;
+	})
+}
+
+// 4-calculate your age in seconds.
+function yourAgeInSeconds(age){
+	return age*365*24*60*60;
+}
+
