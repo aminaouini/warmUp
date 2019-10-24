210f08545f9f8636ef628018cddbb706bf6a0d56
bilel <hb.bilel@gmail.com>
2019-10-24 09:37:30 +0100

    warmup 7

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,23 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+var firstName="maher"
+var lastName = "bouzid"
+var fullName = firstName + " " + lastName 
+
+function number(nb){
+    return nb % 3 === 0
+}
+
+var tab = [13,14,13,15,16,17,19,13,16,15]
+function avgAge(array){
+    var sum = 0
+    for (var i = 0 ; i < tab.length ; i++){
+        sum += tab[i]
+    }
+    return (sum / tab.length)
+}
+
+function ageSecond(age){
+    return "your age is :" + age*356*24*60*60 + " second "
+}
