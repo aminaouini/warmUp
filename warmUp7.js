b153be8af1bcb6e490c20b7cc8302576e6548716
alijalal-sys <ajmabilal@gmail.com>
2019-10-24 09:37:42 +0100

    Warm Up 7 is Done!

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,24 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+console.log('Amira' + ' ' + 'Ahmed');
+
+function multiplOrNot(number){
+	if(number % 3 === 0){
+		return true;
+	}
+	return false;
+}
+
+function averageAge(array){
+	var sum = 0;
+	for(var i = 0; i < array.length; i++){
+		sum += array[i];
+	}
+	return sum / array.length;
+}
+
+function calculateAge(age){
+	return age * 365 * 60 * 60 * 60;
+}
