ac27f718a4cc7e5a45b90d214037f48c180ba647
B.Maher <bouzid.meher@gmail.com>
2019-10-24 09:41:22 +0100

    Warmup 7

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,24 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+// 1-
+var fullName = "first name" + " " + "last name";
+
+//2- 
+//this function return true if the number entred is a multiple of 3; for 13 it return false
+function multipleOf3(n) {
+    return n % 3 === 0;
+}
+
+//3-
+function averageAge(array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++){
+        result += array[i];
+    }
+    return result / array.length;
+}
+
+//4- 
+var myAgeInSeconds = 26 * 365 * 24 * 60 + " Seconds";
