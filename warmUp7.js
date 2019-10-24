fc4c785f543971906f6fba6b592c32fcc8c2771d
AnesMK <marzoukianas@gmail.com>
2019-10-24 09:36:24 +0100

    multipleOfThree chech if n is a multiple of three

--- warmUp7.js
+++ warmUp7.js
@@ -3,6 +3,14 @@ function partnerFullName(first, last) {
 	return first + ' ' + last + '.'
 }
 // 2-find if the number 13 is a multiple of 3 or not.
+function mutipleOfThree(n) {
+	var mod = n % 3;
+	if (mod === 0) {
+		return true;
+	}else {
+		return false;
+	}
+}
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
