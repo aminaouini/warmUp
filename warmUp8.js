23a82a2c4ebb9d25ee973b658b0da55a20709f84
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-10-25 09:35:18 +0100

    Solved warmUp8

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,28 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+
+
+
+
+function isSubset(arr, sub){
+
+	var acc = [];
+
+	for (var i = 0; i <sub.length; i++) {
+		for (var j = 0; j < arr.length; j++) {
+
+		   	if (sub[i]===arr[j]){
+		   		acc.push(sub[i])
+			} 
+
+	   	}
+	}
+
+	if (acc.length===sub.length) {
+		return true;
+	}else{
+		return false
+	}
+}
