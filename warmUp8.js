16857989e1da49b76222de2202495ef6e94adfae
alijalal-sys <ajmabilal@gmail.com>
2019-10-25 09:49:34 +0100

    Warm up 8

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,22 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function isSubset(array1, array2){
+
+	for(var i = 0; i < array2.length; i++){
+		for(var j = 0; j < array1.length; j++){
+			if(array2[i] === array1[j]){
+				array2.splice(i, 1);
+			}
+		}
+	}
+
+	// I don't know what i am doing wrong logically it should work
+
+	if(array2 === []){
+		return true;
+	}
+
+	return false;
+}
