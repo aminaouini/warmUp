2e4d56a06fac131652864c1f495a16ec7460b913
amineslaimia <medamineslaimia@gmail.com>
2019-10-25 09:56:05 +0100

    warmup8

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,21 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+function isSubset (array1,array2){
+    var j=0;
+     for (var i = 0;i <=array1.length;i++){
+         for (var j=0;j<=array2.length;j++){
+             if(array1[i]===array2[j]){
+                 array1.splice(i,1);
+                 array2.splice(j,1);
+                
+                 break;
+             }
+         }
+     }
+                 array1.splice(i-1,1);
+                 array2.splice(j-1,1);
+ 
+     
+     return (array2.length===0)
+ }
