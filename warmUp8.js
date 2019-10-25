cb04496714b50fe71edfa9d60106ad90a0d7d7be
unknown <benabdelaaliadnen@gmail.com>
2019-10-25 10:49:36 +0200

    done !

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,18 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function(arr1 , arr2){
+	var value = false;
+	for (var i = 0 ; i < arr2.length; i++ {
+		
+			if(arr2[i] - arr1[i]){
+              value = true;
+			}
+			else{
+				value = false;
+			}
+		
+	}
+	console.log("the result of second array subset the first array is : "+value);
+}
