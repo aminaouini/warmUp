05c36e9d899758de94132c19bc6d29ae1644c1db
SofianSaleh <sofiane.saleh11@gmail.com>
2019-10-25 10:56:37 +0200

    done

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,37 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function subset(array1, array2) {
+
+	var sub = false;
+	var array3 = []
+
+	if(array1.length < array2.length) {
+
+		return 'Array 1 cannot be a subset of Array 2'
+
+	}
+
+	 for (var i = 0; i < array1.length; i++) {
+
+		for (var j = 0; j < array2.length; j++) {
+
+			if(array1[i] === array2[j]) {
+
+			array3.push(array1[i]);
+
+			}
+
+		}
+
+	}
+
+	if(array3.length === array2.length || array3.length === array1.length ) {
+
+		sub = true;
+
+	}
+	
+	return sub;
+}
