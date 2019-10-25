2190a766d6bf083fc671dc325bc5bd07793f91af
NianaNina <rahmanions@gmail.com>
2019-10-25 09:57:36 +0100

    “yourMessage”

--- warmUp8.js
+++ warmUp8.js
@@ -1,4 +1,30 @@
 //create a function isSubset that should take two arrays and determine whether the second array is a subset of the first array. 
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
-//              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+
+
+
+/*Answer*/
+
+
+function isSubset(arr1, arr2){
+
+    for (var [n] of arr2){
+    	if(!arr1.has(n)){
+    		return false;
+    	}
+    }
+    return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
