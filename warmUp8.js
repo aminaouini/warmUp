b1b0ca2d94fd63db2b519b7af97f89b0c9376a4c
B.Maher <bouzid.meher@gmail.com>
2019-10-25 09:49:07 +0100

    Warmup 8

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,27 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function each(array, f){
+    for(var i = 0; i < array.length; i++){
+        f(array[i],i)
+    }
+}
+
+function reduce(array, f, initial){
+    var acc = initial;
+    each(array, function(elm){
+        acc = f(acc, elm);
+    });
+    return acc; 
+}
+
+//I used array.indexOf() to find if the elements of the sub array are present inside the big array or not.
+function isSubset(array, sub){
+    if (sub.length >= array.length) {
+        return false;
+    }
+    return reduce(sub, function(acc, elm){
+        return acc && (array.indexOf(elm) > -1);
+    },true)
+}
