0f64818336de9ed82ed1acecc6a371ec33db4fb1
muftah <muftah.elzawi@gmail.com>
2019-10-25 10:50:11 +0200

    eighthPushWarmUp

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,49 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
+}
+
+function filter(array, predicate) {
+  var acc = [];
+  each(array, function(element, i) {
+    if (predicate(element, i)) {
+      acc.push(element);
+    }
+  });
+  return acc;
+}
+
+function map(array, func) {
+  var acc = [];
+  each(array, function(element, i) {
+    acc.push(func(element, i));
+  });
+  return acc;
+}
+
+
+
+function isSubset (array1, array2) {
+var	array = []
+
+for (var i = 0; i < array1.length; i++) {
+	for (var j = 0; j < array2.length; j++) {
+		if (array1[i] !== array2[j]) {
+	return false
+		}else {
+			return true
+		}
+	}
+}
+
+}
