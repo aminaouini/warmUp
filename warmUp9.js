f4e067b7f5ff4b3f0f9585f068c00a5791e994fe
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-10-26 13:13:00 +0200

    9.1

--- warmUp9.js
+++ warmUp9.js
@@ -23,4 +23,46 @@
 		font-family: initial;
 	}
 
+// the html file
+
+<!DOCTYPE html>
+<html>
+<header>
+
+	<title>WarmUp9</title>
+
+<meta charset="utf-8">
+
+</header>
+
+<body>
+	
+		<link rel="stylesheet" href="CSSFile.css">
+
+
+	<img src="https://cdn3.movieweb.com/i/article/oQEiGsAQ9p7dacbYWJkBghluePSFbj/798:50/The-Godfather-Book-50th-Anniversary-Francis-Ford-Coppola.jpg" alt="you are seeing this because there shoulb be an img here">
+
+	<img src="https://www.dccomics.com/sites/default/files/GalleryChar_1900x900_MOS_52e05e3fe24a61.04593858.jpg" alt="you are seeing this because there shoulb be an img here">
+
+	<img src="https://images-na.ssl-images-amazon.com/images/I/515wjJQt2nL._SY445_.jpg" alt="you are seeing this because there shoulb be an img here">
+
+	<button class="button1" id='1' name="press 1"> <a href="https://www.github.com">github</a> </button>
+
+	<button class="button2" id='2' name="press 2"> <a href="https://www.bbc.com">bbc</a> </button>
+
+	<ul>
+
+		<li> <a href="https://www.google.com">searching</a> </li>
+		<li> <a href="https://www.amazon.com">shopping</a> </li>
+		<li> <a href="https://edition.cnn.com">news</a> </li>
+
+	</ul>
+
+</body>
+
+<footer>
+	<p>&copyMade by Abdlrrahman Shibani</p>
+
+</footer>
+</html>
 
