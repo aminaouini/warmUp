ae5fc541a0adfac422206b48d5ade5eb7004d26c
AnesMK <marzoukianas@gmail.com>
2019-10-26 09:20:10 +0100

    recommited

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,24 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+
+
+
+
+/*
+function isSubset(array, sub) {
+	
+	for (i=0; i < array.length; i++) {
+		if (sub[0] === array[i]) {
+		sub.slice(0, 1);
+		console.log(sub)	
+	}
+	if (sub.length === 0) {
+	return true;
+	}else {
+	return false
+    }
+	}
+}
+*/
