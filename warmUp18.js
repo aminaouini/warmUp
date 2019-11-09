3bd45078128ca769eabbadd92c10b4c254b1544f
amineaouini <amineaouini565@gmail.com>
2019-11-09 09:54:29 +0100

    ss

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,66 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function warmup18(string) {
+	var a = 0;
+	var d = 0;
+	var e = 0;
+	var h = 0;
+	var l = 0;
+	var r = 0;
+	var o = 0;
+	var w = 0;
+	var __ = 0;
+	var _ = 0;
+	var result = "";
+	for (var i = 0; i < string.length; i++) {
+		if(typeof string[i] ===  "string"){
+			if (string[i] === ",") {
+				_ = _ + 1;
+				result += _;
+			}
+			if (string[i] === " ") {
+				__ = __ + 1;
+				result += __;
+			}
+			if (string[i] === "a") {
+				a = a + 1;
+				result += a;
+			}
+			if (string[i] === "d") {
+				d = d + 1;
+				result += d;
+			}
+			if (string[i] === "e") {
+				e = e + 1;
+				result += e;
+			}
+			if (string[i] === "h") {
+				h = h + 1;
+				result += h;
+			}
+			if (string[i] === "l") {
+				l = l + 1;
+				result += l;
+			}
+			if (string[i] === "r") {
+				r = r + 1;
+				result += r;
+			}
+			if (string[i] === "o") {
+				o = o + 1;
+				result += o;
+			}
+			if (string[i] === "w") {
+				w = w + 1;
+				result += w;
+			}
+		}
+	}
+	return result;
+}
+
+
+
+// LOGIC : for every new letter i have to make it counting for every similar letter.
